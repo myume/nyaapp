df02c93db0ac8e89ff4c3d428b623c1eed93fbcf
myu <contact@memyu.com>
2025-07-15 14:42:46 -0400

    feat: setup flake env for development

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Development environment for the nyaapp";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          at-spi2-atk
+          atkmm
+          cairo
+          gdk-pixbuf
+          glib
+          gtk3
+          harfbuzz
+          librsvg
+          libsoup_3
+          pango
+          webkitgtk_4_1
+          openssl
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          gobject-introspection
+          cargo
+          cargo-tauri
+          nodejs
+        ];
+      };
+    });
+}
+
