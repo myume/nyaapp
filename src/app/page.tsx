8b1aba5b80bdc95e3dd41b33c159b65640dec501
myu <contact@memyu.com>
2025-07-23 23:54:29 -0400

    feat: add frontend logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { invoke } from "@tauri-apps/api/core";
+import { info } from "@tauri-apps/plugin-log";
 
 export default function Home() {
   return (
     <div>
-      <button onClick={() => invoke("search", { query: "c=3_1" })}>
+      <button
+        onClick={async () => {
+          const results = await invoke("search", { query: "c=3_1" });
+          info(JSON.stringify(results));
+        }}
+      >
         hello
       </button>
     </div>
