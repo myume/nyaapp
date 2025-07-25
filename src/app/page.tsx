e040130b77f03f857b51f10face6d92954868221
myu <contact@memyu.com>
2025-07-24 21:04:07 -0400

    feat: setup initial frontend project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-import { invoke } from "@tauri-apps/api/core";
-import { info } from "@tauri-apps/plugin-log";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <button
-        onClick={async () => {
-          const results = await invoke("search", { query: "c=3_1" });
-        }}
-      >
-        hello
-      </button>
-    </div>
-  );
+  redirect("/library");
 }
