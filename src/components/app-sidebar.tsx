a67149189a13e88e8af22046e9279f8aa749c60a
myu <contact@memyu.com>
2025-07-24 22:01:42 -0400

    feat: setup sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,56 @@
+import { Library, Search, Download } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Library",
+    url: "/library",
+    icon: Library,
+  },
+  {
+    title: "Browse",
+    url: "/browse",
+    icon: Search,
+  },
+  {
+    title: "Downloads",
+    url: "/downloads",
+    icon: Download,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Nyaapp</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
