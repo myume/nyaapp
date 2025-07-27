99a8bc3da47532ffb3fde27f786bcbf95d71d0fb
myu <contact@memyu.com>
2025-07-27 13:56:13 -0400

    fix: fix nested buttons in header

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -101,14 +101,12 @@ export default function Browse() {
   return (
     <div className="w-full h-full">
       <div className="sticky top-0 flex justify-between items-center mb-3 gap-5 bg-background transition-all duration-200">
-        <button
-          className="cursor-pointer"
+        <PageHeader
+          title={"Browse"}
           onClick={() => {
             window.scrollTo({ top: 0, behavior: "smooth" });
           }}
-        >
-          <PageHeader title={"Browse"} />
-        </button>
+        />
         <SourceSearch
           setQueryAction={(q) => {
             setQuery(q);
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,8 +1,25 @@
+import { Button } from "./ui/button";
 import { SidebarTrigger } from "./ui/sidebar";
 
-export const PageHeader = ({ title }: { title: string }) => (
+export const PageHeader = ({
+  title,
+  onClick,
+}: {
+  title: string;
+  onClick?: () => void;
+}) => (
   <div className="flex items-center py-3">
     <SidebarTrigger />
-    <h1 className="text-xl">{title}</h1>
+    {onClick ? (
+      <Button
+        variant="ghost"
+        className="cursor-pointer h-full p-1"
+        onClick={onClick}
+      >
+        <h1 className="text-xl">{title}</h1>
+      </Button>
+    ) : (
+      <h1 className="text-xl p-1">{title}</h1>
+    )}
   </div>
 );
