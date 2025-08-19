48aa03928a8d49c6b0812645f212f35721907568
myu <contact@memyu.com>
2025-08-18 23:58:27 -0400

    feat: move navigation tools to the side

--- src/components/ReaderToolbar.tsx
+++ src/components/ReaderToolbar.tsx
@@ -20,77 +20,79 @@ export const ReaderToolbar = ({
       <h1 className="flex-1 truncate overflow-ellipsis">
         {readerContext.libraryEntry?.files[readerContext.fileIndex ?? 0]}
       </h1>
-      <div className="flex flex-1 items-center justify-center gap-2">
-        <Button
-          variant="outline"
-          disabled={currentPage <= 0}
-          onClick={() => setCurrentPageAction(Math.max(currentPage - 1, 0))}
-        >
-          <ChevronLeft />
-        </Button>
-        <div className="flex flex-col justify-center items-center text-xs">
-          <h2>Page</h2>
-          <h4>
-            {currentPage + 1} / {numPages}
-          </h4>
+      <div className="flex gap-4">
+        <div className="flex flex-1 items-center justify-center gap-2">
+          <Button
+            variant="outline"
+            disabled={currentPage <= 0}
+            onClick={() => setCurrentPageAction(Math.max(currentPage - 1, 0))}
+          >
+            <ChevronLeft />
+          </Button>
+          <div className="flex flex-col justify-center items-center text-xs whitespace-nowrap">
+            <h2>Page</h2>
+            <h4>
+              {currentPage + 1} / {numPages}
+            </h4>
+          </div>
+          <Button
+            variant="outline"
+            disabled={currentPage >= numPages - 1}
+            onClick={() =>
+              setCurrentPageAction(Math.min(currentPage + 1, numPages - 1))
+            }
+          >
+            <ChevronRight />
+          </Button>
         </div>
-        <Button
-          variant="outline"
-          disabled={currentPage >= numPages - 1}
-          onClick={() =>
-            setCurrentPageAction(Math.min(currentPage + 1, numPages - 1))
-          }
-        >
-          <ChevronRight />
-        </Button>
-      </div>
-      <div className="flex flex-1 items-center justify-end gap-2">
-        <Button
-          disabled={readerContext.fileIndex === 0}
-          variant="outline"
-          onClick={() =>
-            setReaderContext((context) => ({
-              ...context,
-              fileIndex:
-                context.fileIndex !== undefined
-                  ? Math.max(context.fileIndex - 1, 0)
-                  : 0,
-            }))
-          }
-        >
-          <ChevronLeft />
-        </Button>
-        <div className="flex flex-col justify-center items-center text-xs">
-          <h2>File</h2>
-          <h4>
-            {(readerContext.fileIndex ?? 0) + 1} /{" "}
-            {readerContext.libraryEntry?.files.length}
-          </h4>
+        <div className="flex flex-1 items-center justify-end gap-2">
+          <Button
+            disabled={readerContext.fileIndex === 0}
+            variant="outline"
+            onClick={() =>
+              setReaderContext((context) => ({
+                ...context,
+                fileIndex:
+                  context.fileIndex !== undefined
+                    ? Math.max(context.fileIndex - 1, 0)
+                    : 0,
+              }))
+            }
+          >
+            <ChevronLeft />
+          </Button>
+          <div className="flex flex-col justify-center items-center text-xs whitespace-nowrap">
+            <h2>File</h2>
+            <h4>
+              {(readerContext.fileIndex ?? 0) + 1} /{" "}
+              {readerContext.libraryEntry?.files.length}
+            </h4>
+          </div>
+          <Button
+            disabled={
+              readerContext.libraryEntry?.files.length !== undefined &&
+              readerContext.fileIndex ===
+                readerContext.libraryEntry?.files.length - 1
+            }
+            variant="outline"
+            onClick={() =>
+              setReaderContext((context) => ({
+                ...context,
+                fileIndex:
+                  context.fileIndex !== undefined
+                    ? Math.min(
+                        context.fileIndex + 1,
+                        context.libraryEntry?.files.length !== undefined
+                          ? context.libraryEntry.files.length - 1
+                          : 0,
+                      )
+                    : 0,
+              }))
+            }
+          >
+            <ChevronRight />
+          </Button>
         </div>
-        <Button
-          disabled={
-            readerContext.libraryEntry?.files.length !== undefined &&
-            readerContext.fileIndex ===
-              readerContext.libraryEntry?.files.length - 1
-          }
-          variant="outline"
-          onClick={() =>
-            setReaderContext((context) => ({
-              ...context,
-              fileIndex:
-                context.fileIndex !== undefined
-                  ? Math.min(
-                      context.fileIndex + 1,
-                      context.libraryEntry?.files.length !== undefined
-                        ? context.libraryEntry.files.length - 1
-                        : 0,
-                    )
-                  : 0,
-            }))
-          }
-        >
-          <ChevronRight />
-        </Button>
       </div>
     </div>
   );
