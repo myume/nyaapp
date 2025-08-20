aee95dead656e8e2d90a5c91c6766d802526854c
myu <contact@memyu.com>
2025-08-20 18:31:33 -0400

    feat: add clear all reading progress button

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -4,9 +4,15 @@ import { LibraryCard } from "@/components/LibraryCard";
 import { LibraryDetails } from "@/components/LibraryDetails";
 import { useReader } from "@/components/providers/ReaderProvider";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { LibraryEntry } from "@/types/LibraryEntry";
 import { invoke } from "@tauri-apps/api/core";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, RotateCcw, Settings } from "lucide-react";
 import { redirect } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 
@@ -47,15 +53,37 @@ export default function Library() {
     <div className="flex flex-wrap gap-5">
       {selectedEntry ? (
         <div className="space-y-5">
-          <div className="flex items-center gap-3">
-            <Button
-              variant="outline"
-              onClick={() => setSelectedEntry(undefined)}
-            >
-              <ArrowLeft />
-              Back
-            </Button>
-            <h1>{selectedEntry.name}</h1>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-3">
+              <Button
+                variant="outline"
+                onClick={() => setSelectedEntry(undefined)}
+              >
+                <ArrowLeft />
+                Back
+              </Button>
+              <h1>{selectedEntry.name}</h1>
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="hover:bg-background hover:cursor-pointer p-1 rounded-full aspect-square transition-colors duration-200">
+                <Settings size={20} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mx-2">
+                <DropdownMenuItem
+                  onClick={async () => {
+                    await invoke("clear_reading_progress", {
+                      id: selectedEntry.metafile.source.id,
+                    });
+                    fetchLibrary();
+                  }}
+                >
+                  <RotateCcw className="text-red-400" />
+                  <h1 className="text-red-400 hover:text-red-400">
+                    Reset ALL Reading Progress
+                  </h1>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
           <LibraryDetails
             libraryEntry={selectedEntry}
