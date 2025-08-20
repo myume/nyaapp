e2586821f06d62be9c4affba662cc695e08af77b
myu <contact@memyu.com>
2025-08-20 18:48:17 -0400

    feat: disable clicking into library detail while downloading

--- src/components/LibraryCard.tsx
+++ src/components/LibraryCard.tsx
@@ -8,6 +8,8 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { useDownloads } from "./providers/DownloadsProvider";
+import { cn } from "@/lib/utils";
 
 export const LibraryCard = ({
   libraryEntry,
@@ -25,12 +27,25 @@ export const LibraryCard = ({
       metadata,
     },
   } = libraryEntry;
+  const { downloads } = useDownloads();
+  const downloadInfo = downloads[id];
+  const isDownloading = downloadInfo && !downloadInfo.finished;
 
   return (
-    <div className="flex flex-col items-center justify-between border-1 rounded-xl p-4 w-52 gap-2 hover:bg-muted/80 transition-colors duration-200">
+    <div
+      className={cn(
+        "flex flex-col items-center justify-between border-1 rounded-xl p-4 w-52 gap-2transition-colors duration-200",
+        !isDownloading && "hover:bg-muted/80",
+        isDownloading && "cursor-not-allowed",
+      )}
+    >
       <div
-        className="hover:cursor-pointer"
-        onClick={() => setSelectedAction(libraryEntry)}
+        className={cn(
+          !isDownloading ? "hover:cursor-pointer" : "hover:cursor-not-allowed",
+        )}
+        onClick={() => {
+          if (!isDownloading) setSelectedAction(libraryEntry);
+        }}
       >
         {metadata?.cover && (
           <Image
@@ -46,23 +61,30 @@ export const LibraryCard = ({
           <h1>{name}</h1>
         </div>
       </div>
-      <div className="flex justify-end w-full">
-        <DropdownMenu>
-          <DropdownMenuTrigger className="hover:bg-background hover:cursor-pointer p-1 rounded-full aspect-square transition-colors duration-200">
-            <EllipsisVertical />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem
-              onClick={async () => {
-                await invoke("delete", { id });
-                onDeleteAction(id);
-              }}
-            >
-              <Trash /> Delete
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+      {!isDownloading && (
+        <div className="flex justify-end w-full">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="hover:bg-background hover:cursor-pointer p-1 rounded-full aspect-square transition-colors duration-200">
+              <EllipsisVertical />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                onClick={async () => {
+                  await invoke("delete", { id });
+                  onDeleteAction(id);
+                }}
+              >
+                <Trash /> Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )}
+      {isDownloading && (
+        <div className="text-sm">
+          {`Downloading ${Math.round((downloadInfo.progress_bytes / Math.max(downloadInfo.total_bytes, 1)) * 100)}%`}
+        </div>
+      )}
     </div>
   );
 };
